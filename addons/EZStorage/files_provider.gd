66866fdb87565b31510f11c8ca3c9d08c6fe432c
Matthew Murphy <mashumafi@users.noreply.github.com>
2022-09-01 23:05:55 -0400

    Add an LRU cache to the files provider (#18)

--- addons/EZStorage/files_provider.gd
+++ addons/EZStorage/files_provider.gd
@@ -1,22 +1,160 @@
 extends "storage_provider.gd"
 
 const Util := preload("util.gd")
+
 var decoder := {
 	"s": StoreCommand,
 	"p": PurgeCommand,
 }
+var cache := Cache.new(30)
+
+
+class ObjectFactory:
+	var cache := []
+	var type
+
+	func _init(type):
+		self.type = type
+
+	func _notification(what: int) -> void:
+		if what == NOTIFICATION_PREDELETE:
+			for obj in cache:
+				obj.free()
+
+	func alloc() -> Object:
+		if cache.empty():
+			return self.type.call("new")
+
+		return cache.pop_back()
+
+	func has(inst: Object) -> bool:
+		for obj in cache:
+			if obj == inst:
+				return true
+		return false
+
+	func delete(obj: Object):
+		assert(not has(obj), "Already deleted the obj")
+		cache.push_back(obj)
+
+
+class Cache:
+	extends Reference
+
+	var head: Link
+	var tail: Link
+	var lookup: Dictionary
+	var size: int
+	var links := ObjectFactory.new(Link)
+
+	class Link:
+		extends Object
+
+		var next: Link
+		var prev: Link
+		var key
+		var data
+
+		func _notification(what: int) -> void:
+			if what == NOTIFICATION_PREDELETE:
+				if next:
+					next.free()
+
+	func _init(size: int):
+		self.size = size
+
+	func _notification(what: int) -> void:
+		if what == NOTIFICATION_PREDELETE:
+			if head:
+				head.free()
+
+	func get(key):
+		var link: Link = lookup.get(key)
+		if link:
+			if link != head and lookup.size() > 1:
+				_remove(key)
+				_insert(link)
+
+			return link.data
+
+	func _insert(link: Link):
+		if head == null:
+			tail = link
+		else:
+			head.prev = link
+
+		link.next = head
+		head = link
+
+	func insert(key, data):
+		assert(not lookup.has(key), "The key already exists.")
+		var link: Link = links.alloc()
+		link.key = key
+		link.data = data
+		lookup[key] = link
+		_insert(link)
+		if lookup.size() > size:
+			_delete_last()
+
+	func _remove(key) -> Link:
+		var current: Link = lookup.get(key)
+		if not current:
+			return null
+
+		if current == head:
+			head = head.next
+		else:
+			current.prev.next = current.next
+
+		if current == tail:
+			tail = current.prev
+		else:
+			current.next.prev = current.prev
+
+		return current
+
+	func _delete_last():
+		var link := tail
+
+		if head.next == null:
+			head = null
+		else:
+			tail.prev.next = null
+
+		tail = tail.prev
+
+		lookup.erase(link.key)
+		link.next = null
+		links.delete(link)
+
+	func erase(key):
+		var current := _remove(key)
+		if current:
+			lookup.erase(key)
+			current.next = null
+			links.delete(current)
 
 
 func read_section(section: String) -> Dictionary:
-	var path := root.plus_file(Util.hash_filename(section))
+	section = Util.hash_filename(section)
+	var cached_section = cache.get(section)
+	if cached_section != null:
+		return cached_section
+
+	var path := root.plus_file(section)
 	var directory := Directory.new()
 	if directory.file_exists(path):
 		var file := File.new()
 		file.open(path, File.READ)
 		var contents := bytes2var(file.get_buffer(file.get_len())) as Dictionary
 		if contents:
+			cache.insert(section, contents)
 			return contents
-	return {}
+
+	# Cache that this file did not exist
+	var keys := {}
+	cache.insert(section, keys)
+	return keys
 
 
 class StoreCommand:
@@ -92,6 +230,7 @@ func purge(skip_sections: PoolStringArray) -> bool:
 	for dir in dirs:
 		if skip_sections.has(dir):
 			continue
+		cache.erase(dir)
 		var command := PurgeCommand.new(dir)
 		all_success = Util.execute(get_root(), command) and all_success
 	return all_success
@@ -104,10 +243,12 @@ func purge_section(section: String, skip_keys: PoolStringArray) -> bool:
 			continue
 		keys.erase(key)
 	var command: Util.Command
+	section = Util.hash_filename(section)
 	if keys.empty():
-		command = PurgeCommand.new(Util.hash_filename(section))
+		cache.erase(section)
+		command = PurgeCommand.new(section)
 	else:
-		command = StoreCommand.new(Util.hash_filename(section), keys)
+		command = StoreCommand.new(section, keys)
 	return Util.execute(get_root(), command)
 
 
